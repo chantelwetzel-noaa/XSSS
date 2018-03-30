8207d8e25b93f8a494351c7ec98b6fa37cd6924e
Chantel Wetzel <chantel.wetzel@noaa.gov>
2018-03-30 15:52:04 -0700

    Working call change

--- examples/call_Example.R
+++ examples/call_Example.R
@@ -1,11 +1,8 @@
 
 
-
-source("C:/Users/Chantel.Wetzel/Documents/GitHub/XSSS_AIS/R/Do_XSSS_AIS.R")
-
 SSS.ais.fxn(filepath = "C:/My_Directory/.../", # parent directory to the file.name folder 
-            control.name = "ENGL.ctl", 
-            dat.name = "ENGL.dat",
+            control.name = "control_file.ctl", 
+            dat.name = "data_file.dat",
             m.in = c(0.28, 0.27,0.40,0.40, 0), 
             h.in = c(0.87, 0.093, 0.20, 1, 1), 
             depl.in = c(0.60, 0.10, 0.01, 0.99, 1) )
@@ -14,7 +11,8 @@ SSS.ais.fxn(filepath = "C:/My_Directory/.../", # parent directory to the file.na
 SSS.ais.fxn(filepath = "C:/Users/Chantel.Wetzel/Desktop/pop_ais", # parent directory to the file.name folder 
             control.name = "pop.ctl" ,
             dat.name = "pop.dat",
-            Niter = 2000, AIS.iter = 2000, final.Niter = 5000,
+            entropy.level = 0.80,
+            Niter = 200, AIS.iter = 200, final.Niter = 200,
             m.in = c(0.054, 0.054,0.20,0.20, 0) ,
             h.in = c(0.50, 0.093, 0.20, 1, 1) ,
             depl.in = c(0.20, 0.10, 0.01, 0.99, 1))
@@ -26,7 +24,12 @@ dat.name = "pop.dat"
 m.in = c(0.054, 0.054,0.20,0.20, 0) 
 h.in = c(0.50, 0.093, 0.20, 1, 1) 
 depl.in = c(0.20, 0.10, 0.01, 0.99, 1)
-Niter = 50 
-AIS.iter = 50
-final.Niter = 400
+Niter = 200 
+AIS.iter = 200
+final.Niter = 200
+tantalus=FALSE 
+read.seed = FALSE
+entropy.level = 0.80
+fmsy.m.in = NULL
+bmsy.b0.in = NULL
 
