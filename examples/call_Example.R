db6b4fd1e46a79bda3be1305b4cf9c0a1e119e4c
Chantel Wetzel <chantel.wetzel@noaa.gov>
2018-04-03 08:13:11 -0700

    Updated call example

--- examples/call_Example.R
+++ examples/call_Example.R
@@ -1,35 +1,30 @@
 
-
+# Generic call to function 
+# Only the required inputs are specified here
 SSS.ais.fxn(filepath = "C:/My_Directory/.../", # parent directory to the file.name folder 
+            control.name = "control_file.ctl", # name of the model control file
+            dat.name = "data_file.dat", # name of the model data file
+            m.in = c(0.28, 0.27,0.40,0.40, 0), # c(female M median value, male M median value, female M sd, male M sd, M equal between sexes (0=No, 1=Yes))
+            h.in = c(0.87, 0.093, 0.20, 1, 1), # c(steepness, mean, steepness sd, lower bound, upper bound, distribution (1= beta))
+            depl.in = c(0.60, 0.10, 0.01, 0.99, 1) #c(depletion in select year, sd, lower bound, upper bound, distribution (1 = beta, 2 = lognormal))
+            )
+
+
+# Function call with all inputs specified
+SSS.ais.fxn(filepath = "C:/My_Directory/.../", 
             control.name = "control_file.ctl", 
-            dat.name = "data_file.dat",
+            dat.name = "data_file.dat", 
+            tantalus = FALSE, 
+            read.seed = FALSE, # Option to allow the user to predefine seeds
+            entropy.level = 0.92, # Level of entropy to meet before finishing AIS
+            Niter = 2000, # Initial model run for MCMC
+            AIS.iter = 2000, # Interum run size for AIS
+            final.Niter = 5000, # Final model sample size
             m.in = c(0.28, 0.27,0.40,0.40, 0), 
             h.in = c(0.87, 0.093, 0.20, 1, 1), 
-            depl.in = c(0.60, 0.10, 0.01, 0.99, 1) )
+            depl.in = c(0.60, 0.10, 0.01, 0.99, 1), 
+            fmsy.m.in = NULL, # Not yet fully implemented
+            bmsy.b0.in = NULL # Not yet fully implemented
+            ) 
 
-
-SSS.ais.fxn(filepath = "C:/Users/Chantel.Wetzel/Desktop/pop_ais", # parent directory to the file.name folder 
-            control.name = "pop.ctl" ,
-            dat.name = "pop.dat",
-            entropy.level = 0.80,
-            Niter = 200, AIS.iter = 200, final.Niter = 200,
-            m.in = c(0.054, 0.054,0.20,0.20, 0) ,
-            h.in = c(0.50, 0.093, 0.20, 1, 1) ,
-            depl.in = c(0.20, 0.10, 0.01, 0.99, 1))
-
-
-filepath = "C:/Users/Chantel.Wetzel/Desktop/pop_ais" # parent directory to the file.name folder 
-control.name = "pop.ctl" 
-dat.name = "pop.dat"
-m.in = c(0.054, 0.054,0.20,0.20, 0) 
-h.in = c(0.50, 0.093, 0.20, 1, 1) 
-depl.in = c(0.20, 0.10, 0.01, 0.99, 1)
-Niter = 200 
-AIS.iter = 200
-final.Niter = 200
-tantalus=FALSE 
-read.seed = FALSE
-entropy.level = 0.80
-fmsy.m.in = NULL
-bmsy.b0.in = NULL
 
